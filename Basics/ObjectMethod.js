b8ee8c11a0b94355b23ba2a137219785b6dbafd0
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2021-02-09 23:40:21 +0900

    Update ObjectMethod.js

--- Basics/ObjectMethod.js
+++ Basics/ObjectMethod.js
@@ -13,6 +13,22 @@ var circle = {
     area : function(){
         return Math.PI * Math.pow(this.radius,2)
     } 
-}
+};
 
 console.log(circle.area())
+
+/*
+메소드 또한 프로퍼티의 일종이므로 나중에 추가해줄 수 있다.
+*/
+
+circle.move = function(a,b){
+    this.center.x += a;
+    this.center.y += b
+};
+
+circle.move(4,5)
+console.log(circle.center)
+
+/*
+여기서 알 수 있듯이 메소드는 일반적으로 메소드가 속한 객체의 내부 프로퍼티 값의 상태를 바꾸는 용도로 주로 사용한다.
+*/
